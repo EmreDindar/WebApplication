67af41377a20373c9b8307ce143c05476d3838c2
Emre Dindar <emredindar93@hotmail.com>
2020-02-13 16:09:56 +0300

    initial commit

---
+++ src/main/java/com/emre/WebApplication/SpringBootFirstWebApplication.java
@@ -0,0 +1,14 @@
+package com.emre.WebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.emre.WebApplication")
+public class SpringBootFirstWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstWebApplication.class, args);
+	}
+}
